390c7b2242777f3106f320b1fc88682ded1cb3ba
RoozbehZhuleh <zhuleh.developer@gmail.com>
2022-05-30 09:33:38 +0430

    update app.js

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
 "use strict";
 
 // selectors
+const clear = document.querySelector(".clear");
 const inputNumber = document.querySelector(".input-number");
 const generalKeys = document.querySelectorAll(".gen");
 const operators = document.querySelectorAll(".operator");
 const numbers = document.querySelectorAll(".number");
-const equals = document.querySelector(".equals");
+const resultEquals = document.querySelector(".equals");
+const resultsLine = document.querySelector(".results-line");
+const percent = document.querySelector(".percent");
+let percentage;
 
 
-function getResult() {
-    console.log(inputNumber.value);
-}
+// function getResult() {
+//     console.log(inputNumber.value);
+// }
 
+// equals.addEventListener("click", getResult);
+
+clear.addEventListener("click", () => {
+    inputNumber.value = "";
+})
 
 numbers.forEach((i) => {
     i.addEventListener("click", () => {
         inputNumber.value = inputNumber.value + i.textContent;
-        inputNumber.value 
     });
 })
 
+operators.forEach((o) => {
+    o.addEventListener("click", () => {
+         inputNumber.value = inputNumber.value + o.textContent;
+    })
+})
 
-equals.addEventListener("click", getResult);
+
+// percent.addEventListener("click", () => {
+//     inputNumber.value = percentage;
+//     console.log(percentage);
+//     inputNumber.value = "";
+// })
+
+
+resultEquals.addEventListener("click", () => {
+    resultsLine.textContent = eval(inputNumber.value);
+})
+
+
