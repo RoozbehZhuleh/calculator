1c998e17db0903e84a3aa01b968bd0da2f8f1d61
RoozbehZhuleh <zhuleh.developer@gmail.com>
2022-05-30 18:49:10 +0430

    update

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const generalKeys = document.querySelectorAll(".gen");
 const operators = document.querySelectorAll(".operator");
 const numbers = document.querySelectorAll(".number");
 const resultEquals = document.querySelector(".equals");
-const resultsLine = document.querySelector(".results-line");
+const pn = document.querySelector(".pn");
+let resultsLine = document.querySelector(".results-line");
 const percent = document.querySelector(".percent");
-let percentage;
+const history = document.querySelector(".history");
+const reverse = document.querySelector(".reverse");
+let percentage1 = inputNumber.value;
+let percent2 = "";
+// let lastResult = "";
 
 
 // function getResult() {
@@ -18,9 +23,14 @@ let percentage;
 
 // equals.addEventListener("click", getResult);
 
-clear.addEventListener("click", () => {
+function clearResults() {
     inputNumber.value = "";
-})
+    resultsLine.textContent = "0";
+
+}
+
+
+clear.addEventListener("click", clearResults);
 
 numbers.forEach((i) => {
     i.addEventListener("click", () => {
@@ -35,15 +45,36 @@ operators.forEach((o) => {
 })
 
 
-// percent.addEventListener("click", () => {
-//     inputNumber.value = percentage;
-//     console.log(percentage);
-//     inputNumber.value = "";
-// })
+pn.addEventListener("click", () => {
+    inputNumber.value = inputNumber.value * -1;
+})
+
+
+percent.addEventListener("click", () => {
+// let percentage1 = inputNumber.value;
+let percentage2 = inputNumber.value;
+    console.log(percentage1, percentage2);
+}
+)
 
 
 resultEquals.addEventListener("click", () => {
-    resultsLine.textContent = eval(inputNumber.value);
+        resultsLine.textContent = eval(inputNumber.value);
+    inputNumber.value = "";
+
+    for (let i = 0; i < 5; i++){
+        var lastResults = lastResults[i];
+        console.log(lastResults);
+    }
+        
+        
+})
+
+
+// history
+
+history.addEventListener("click", () => {
+    lastResult = lastResult + resultsLine.textContent;
 })
 
 
